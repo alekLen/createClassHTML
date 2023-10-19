229c9faee30f5cfe6009dc27fe81f3cfda873ca5
Elena <aleklenoch@gmail.com>
2023-10-19 20:40:00 +0300

    #создание и вложение элементов

--- TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/app.js
@@ -13,6 +13,12 @@ var Greeter = /** @class */ (function () {
     Greeter.prototype.setStyle = function (stName, stValue) {
         this.styles.push("".concat(stName, ":").concat(stValue));
     };
+    Greeter.prototype.setTagToEnd = function (item) {
+        this.includeTags.push(item);
+    };
+    Greeter.prototype.setTagToBegining = function (item) {
+        this.includeTags.unshift(item);
+    };
     Greeter.prototype.getHtml = function () {
         var str = "<".concat(this.name, " ");
         if (this.styles.length > 0) {
@@ -37,7 +43,7 @@ var Greeter = /** @class */ (function () {
         if (!this.selfClosing) {
             if (this.includeTags.length > 0) {
                 this.includeTags.forEach(function (ht) {
-                    ht.getHtml();
+                    str += ht.getHtml();
                 });
             }
             str += "</".concat(this.name, "> ");
@@ -47,19 +53,28 @@ var Greeter = /** @class */ (function () {
     return Greeter;
 }());
 window.onload = function () {
-    var wrapper = new Greeter("div", false, "fdsdfsdfsdfsd");
+    var wrapper = new Greeter("div", false, "");
     wrapper.setStyle("display", "flex");
     wrapper.setAttr("id", "wrapper");
-    document.write(wrapper.getHtml());
     var div1 = new Greeter("div", false, "");
     div1.setStyle("width", "300px");
     div1.setStyle("margin", "10px");
-    var hr = new Greeter("hr", false, "What is Lorem Ipsum?");
+    var h = new Greeter("h3", false, "What is Lorem Ipsum?");
     var img = new Greeter("img", true, "");
     img.setAttr("src", "lipsum.jpg");
     img.setAttr("alt", "Lorem Ipsum");
     img.setStyle("width", "100%");
-    document.write(hr.getHtml());
-    document.write(img.getHtml());
+    var p = new Greeter("p", false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt gravida urna eu viverra. Aliquam vehicula mauris dolor, non consectetur magna pharetra id. Phasellus velit nulla, ultricies ut ipsum in, vestibulum accumsan velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget nisi facilisis, mattis tortor in, feugiat eros. Aliquam porttitor id ante nec feugiat. Phasellus pulvinar libero et maximus finibus. Nullam ultricies quis mauris sit amet hendrerit. In non ligula in nunc dapibus egestas eget eget nisi. Vivamus iaculis magna leo, in posuere est dignissim id. Sed dignissim hendrerit turpis ac posuere. Aliquam vitae accumsan nunc.");
+    p.setStyle("text-align", "justify");
+    var a = new Greeter("a", false, "More...");
+    a.setAttr("href", "https://www.lipsum.com");
+    a.setAttr("target", "_blank");
+    div1.setTagToEnd(img);
+    div1.setTagToEnd(p);
+    div1.setTagToEnd(a);
+    div1.setTagToBegining(h);
+    wrapper.setTagToEnd(div1);
+    wrapper.setTagToEnd(div1);
+    document.write(wrapper.getHtml());
 };
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp1/app.ts
+++ TypeScriptHTMLApp1/app.ts
@@ -18,7 +18,12 @@ class Greeter {
     public setStyle(stName: string, stValue: string): void {
         this.styles.push(`${stName}:${stValue}`);
     }
-
+    public setTagToEnd(item: Greeter): void {
+        this.includeTags.push(item);
+    }
+    public setTagToBegining(item: Greeter): void {
+        this.includeTags.unshift(item);
+    }
     getHtml(): string {
         let str: string = `<${this.name} `;
 
@@ -44,7 +49,7 @@ class Greeter {
         {
            if (this.includeTags.length > 0) {
                 this.includeTags.forEach((ht) => {
-                    ht.getHtml();
+                   str+= ht.getHtml();
                 });
             }
             str += `</${this.name}> `;
@@ -54,20 +59,29 @@ class Greeter {
 }
 
 window.onload = () => {
-    let wrapper: Greeter = new Greeter("div", false, "fdsdfsdfsdfsd");
+    let wrapper: Greeter = new Greeter("div", false, "");
     wrapper.setStyle("display", "flex");
     wrapper.setAttr("id", "wrapper");
-    document.write(wrapper.getHtml());
     let div1: Greeter = new Greeter("div", false, "");
     div1.setStyle("width", "300px");
     div1.setStyle("margin", "10px");
-    let hr: Greeter = new Greeter("hr", false, "What is Lorem Ipsum?");
+    let h: Greeter = new Greeter("h3", false, "What is Lorem Ipsum?");
     let img: Greeter = new Greeter("img", true, "");
     img.setAttr("src", "lipsum.jpg");
     img.setAttr("alt", "Lorem Ipsum");
     img.setStyle("width", "100%");
+    let p: Greeter = new Greeter("p", false, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt gravida urna eu viverra. Aliquam vehicula mauris dolor, non consectetur magna pharetra id. Phasellus velit nulla, ultricies ut ipsum in, vestibulum accumsan velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget nisi facilisis, mattis tortor in, feugiat eros. Aliquam porttitor id ante nec feugiat. Phasellus pulvinar libero et maximus finibus. Nullam ultricies quis mauris sit amet hendrerit. In non ligula in nunc dapibus egestas eget eget nisi. Vivamus iaculis magna leo, in posuere est dignissim id. Sed dignissim hendrerit turpis ac posuere. Aliquam vitae accumsan nunc.");
+    p.setStyle("text-align", "justify");
+    let a: Greeter = new Greeter("a", false, "More...");
+    a.setAttr("href", "https://www.lipsum.com");
+    a.setAttr("target", "_blank");
+    div1.setTagToEnd(img);
+    div1.setTagToEnd(p);
+    div1.setTagToEnd(a);
+    div1.setTagToBegining(h);
+    wrapper.setTagToEnd(div1);
+    wrapper.setTagToEnd(div1);
+    document.write(wrapper.getHtml());
 
-    document.write(hr.getHtml());
-    document.write(img.getHtml());
 
 };
